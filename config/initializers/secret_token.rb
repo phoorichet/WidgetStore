7252e3b74e13a1af154f69c8a41c01531533844d
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2013-11-20 15:18:44 -0800

    Init the widgetstore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WidgetStore::Application.config.secret_token = '17318f2d299536f287e468d525f9367b51e2528de5ce8087f36ac145529265bbbf500dd241afe361b7e5ada47462b72ce9f68b4ae6ae47ed09fe9d36eb377703'
