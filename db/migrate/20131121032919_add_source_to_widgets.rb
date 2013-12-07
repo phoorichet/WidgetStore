ced599a57a96110b308a454fbc58a7b9fd285a3d
Jingyi Li <lijingyics@gmail.com>
2013-12-06 21:08:25 -0800

    delete add source and remove source migration files

--- db/migrate/20131121032919_add_source_to_widgets.rb
+++
@@ -1,9 +0,0 @@
-class AddSourceToWidgets < ActiveRecord::Migration
-  def self.up
-    add_attachment :widgets, :source
-  end
-
-  def self.down
-    remove_attachment :widgets, :source
-  end
-end
--- db/migrate/20131127064301_remove_source_from_widgets.rb
+++
@@ -1,9 +0,0 @@
-class RemoveSourceFromWidgets < ActiveRecord::Migration
-  def change
-    remove_column :widgets, :source_file_name
-    remove_column :widgets, :source_content_type
-    remove_column :widgets, :source_file_size
-    remove_column :widgets, :source_updated_at
-  end
-
-end
